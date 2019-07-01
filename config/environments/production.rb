e2ec70843344508c57f7a656318061aa0de0f234
Sreedev Kodichath <sreedevpadmakumar@gmail.com>
2019-07-01 23:35:19 +0530

    added postfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sreedev_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings   = {
+    address: "sree.dev",
+    port: 25,
+    domain: "sree.dev"
+  }
+  config.action_mailer.default_url_options = { :host => 'sree.dev' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
